b9db1b43f057f412763edef26071bed72986d969
Cirdei Petrisor <petrisorc65@gmail.com>
2021-04-25 17:21:38 +0300

    Add project files.

---
+++ Laborator1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Utilizare valori din fisierul de configurari
+            string titlu =
+            ConfigurationManager.AppSettings.Get("TitluAplicatie");
+            Console.WriteLine(titlu);
+            Console.ReadKey();
+        }
+    }
+}
